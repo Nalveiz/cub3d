2574771f3983d15a67fa5739f3f0cdd7eb16c496
nalveiz <ozemusta44@gmail.com>
2024-12-11 16:02:09 +0300

    map arround control ok

--- utils/ft_map_control.c
+++ utils/ft_map_control.c
@@ -6,7 +6,7 @@
 /*   By: musozer <musozer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 23:22:27 by musozer           #+#    #+#             */
-/*   Updated: 2024/12/07 13:11:45 by musozer          ###   ########.fr       */
+/*   Updated: 2024/12/11 16:00:45 by musozer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ static void ft_wall_check(char *line)
 	while (line[i] && line[i] == ' ')
 		i++;
 	if (line[i] != '1')
-		ft_err_msg("Wron map format");
+		ft_err_msg("satır başı 1 mi");
 	i = ft_strlen(line) -2;
 	while (i >= 0 && line[i] == ' ')
 		i--;
 	if (i >= 0 && line[i] != '1')
-		ft_err_msg("Wrongggg map format");
+		ft_err_msg("satır sonu 1 mi");
 }
 static void	ft_char_check(char *line, int flag)
 {
@@ -40,8 +40,36 @@ static void	ft_char_check(char *line, int flag)
 			&& line[i] != ' ' && line[i] != '\n')
 			ft_err_msg("Wrong ccharacter in map");
 		if (flag == 0 && (line[i] != '1' && line[i] != ' ' && line[i] != '\n'))
-		    ft_err_msg("Wrong map format");
+		    ft_err_msg("ilk satır komple 1 mi");
 		i++;
+	}
+}
+
+static	void	ft_around_map_check(t_data *data, int k)
+{
+	int i;
+	int c;
+
+	while (data->j < data->map->width -1)
+	{
+		c = data->map_len -1;
+		i = k;
+		while (i < c && (data->map->map[i][data->j] == ' ' || data->map->map[i][data->j] == '\n'
+			|| (int)ft_strlen(data->map->map[i]) <= data->j))
+			i++;
+		if (i == c)
+		{
+			data->j++;
+			continue;
+		}
+		if (data->map->map[i][data->j] != '1' && data->map->map[i][data->j])
+			ft_err_msg("sütün başı 1 mi");
+		while (i < c && (data->map->map[c][data->j] == ' '
+			|| data->map->map[c][data->j] == '\n' || (int)ft_strlen(data->map->map[c]) <= data->j))
+			c--;
+		if (data->map->map[c][data->j] != '1' && data->map->map[i][data->j])
+			ft_err_msg("sütün sonu 1 mi");
+		data->j++;
 	}
 }
 
@@ -53,8 +81,11 @@ static	void	ft_map_start_check(t_data *data, char *line)
 		data->flag = 0;
 	data->flag = 1;
 	ft_wall_check(line);
+
+
 	// eğer ki hata yoksa data->map->map[data->i] ' si free lenicek en son map free olucak
 }
+
 
 void	ft_map_check(t_data *data)
 {
@@ -75,6 +106,7 @@ void	ft_map_check(t_data *data)
 		ft_map_start_check(data, data->map->map[data->i]);
 		data->i++;
 	}
+	ft_around_map_check(data,i);
 	// eğer hata yoksa map3d oluşturuluyor ve içine atılıyor
 	data->map->map3d = (char **)malloc(sizeof(char *) * (data->map->height + 1));
 	while (i < data->map_len)
@@ -83,5 +115,5 @@ void	ft_map_check(t_data *data)
 		i++;
 		j++;
 	}
-	// soksakım ana mantık bu şekilde yaptım sonra yine configre ederiz bundan sonra :>)
+	data->map->map3d[j] = NULL;
 }
